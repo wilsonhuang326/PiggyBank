9ea3f153516446cd698eacfeafb4a3e84c2a1d05
hemonJuice <hanwen.ju17@gmail.com>
2021-05-10 11:44:05 -0400

    计算器update2.0

--- app/src/main/java/com/example/piggybank/addTrans.java
+++ app/src/main/java/com/example/piggybank/addTrans.java
@@ -14,8 +14,8 @@ public class addTrans extends AppCompatActivity implements View.OnClickListener{
             btnBackward,btnPlus,btnSubtract,btnMultiply,btnDivide,btnComp,btnPoint,btnDate;
 
     TextView text;
-    String input = "";
-
+    String str = "";
+    boolean clr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +45,7 @@ public class addTrans extends AppCompatActivity implements View.OnClickListener{
         btnComp = (Button) findViewById(R.id.comp);
         btnDate = (Button) findViewById(R.id.date);
 
-
+        text = (TextView) findViewById(R.id.text) ;
 
         btn0.setOnClickListener(this);
         btn1.setOnClickListener(this);
@@ -65,6 +65,149 @@ public class addTrans extends AppCompatActivity implements View.OnClickListener{
         btnPoint.setOnClickListener(this);
         btnComp.setOnClickListener(new click());
         btnDate.setOnClickListener(this);
+
+
+        }
+        class click implements View.OnClickListener{
+            public void onClick(View v) {
+                getResult();
+
+            }
+        }
+
+        public void onClick(View v){
+            String input = text.getText().toString();
+            switch (v.getId()){
+                case R.id.num0:
+                case R.id.num1:
+                case R.id.num2:
+                case R.id.num3:
+                case R.id.num4:
+                case R.id.num5:
+                case R.id.num6:
+                case R.id.num7:
+                case R.id.num8:
+                case R.id.num9:
+                case R.id.dot:
+//                    if (clr == true){
+//                        clr = false;
+//                        str = "";
+//                        text.setText("");
+//                    }
+                    text.setText(input + ((Button)v).getText());
+                    break;
+                case R.id.minus:
+                case R.id.add:
+                case R.id.div:
+                case R.id.mult:
+//                    if (clr == true){
+//                        clr = false;
+//                        str = "";
+//                        text.setText("");
+//                    }
+                    text.setText(input + " " + ((Button)v).getText() + " ");
+                    break;
+                case R.id.chehui:
+                    text.setText("");
+                    break;
+            }
+        }
+
+        private void getResult(){
+
+
+            //text.setText("111");
+
+            String s = text.getText().toString();
+            double result = 0;
+
+            if(s.equals("") || s == null){
+                return;
+            }
+//            if(clr = true){
+//                return;
+//            }
+//            clr = true;
+
+            String s1 = s.substring(0,s.indexOf(" "));
+//        运算符
+            String op = s.substring(s.indexOf(" ")+1,s.indexOf(" ")+2);
+//        运算符后的数字
+            String s2 = s.substring(s.indexOf(" ")+3);
+
+            if(!s1.equals("")&&!s2.equals("")) {
+                double d1 = Double.parseDouble(s1);//则数字都是double类型
+                double d2 = Double.parseDouble(s2);
+                if (op.equals("+")) {   //如果是 +
+                    result = d1 + d2;
+                } else if (op.equals("-")) {   //如果是 -
+                    result = d1 - d2;
+                } else if (op.equals("*")) {   //如果是 *
+                    result = d1 * d2;
+                } else if (op.equals("/")) {   //如果是 /
+                    if (d2 == 0) { //如果被除数是0
+                        result = 0; //则结果是0
+                    } else {//否则执行正常是除法运算
+                        result = d1 / d2;
+                    }
+                }
+
+                if (!s1.contains(".") && !s2.contains(".") && !op.equals("/")) {//如果是整数类型
+                    int r = (int) result; //都是整形
+                    text.setText(r+"");
+                } else{
+                    text.setText(result+"");
+                }
+
+            }
+
+            else if(!s1.equals("")&&s2.equals("")){
+                double d1 = Double.parseDouble(s1);
+                if (op.equals("+")){
+                    result = d1;
+                }
+                if (op.equals("-")) {
+                    result = d1;
+                }
+                if (op.equals("*")) {
+                    result = d1;
+                }
+                if (op.equals("/")) {
+                    result = d1;
+                }
+                if(!s1.contains(".")) {
+                    int res = (int) result;
+                    text.setText(res+"");
+                }else {
+                    text.setText(result+"");
+                }
+            }
+
+            else if(s1.equals("")&& !s2.equals("")){
+                double d2 = Double.parseDouble(s2);
+                if(op.equals("+")){
+                    result = 0+d2;
+                }else if(op.equals("-")){
+                    result = 0-d2;
+                }else if(op.equals("*")){
+                    result = 0;
+                }else if(op.equals("/")){
+                    result = 0;
+                }
+                if(!s1.contains(".") && !s2.contains(".")){
+                    int ress = (int) result;
+                    text.setText(ress + "");
+                }else{
+                    text.setText(result + "");
+                }
+            }else{
+                text.setText("");
+            }
+
+
+        }
+
+
     }
 
 
@@ -105,4 +248,4 @@ public class addTrans extends AppCompatActivity implements View.OnClickListener{
 //    Button date;
 
 
-}
+

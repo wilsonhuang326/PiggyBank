9ac5a4d2a105992e9382652b87770231fea81644
wilson <wilsonhuang0326@gmail.com>
2021-05-12 21:46:08 -0400

    简易

--- app/src/main/java/com/example/piggybank/CategoryManagement.java
+++ app/src/main/java/com/example/piggybank/CategoryManagement.java
@@ -2,6 +2,7 @@ package com.example.piggybank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -10,12 +11,13 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ListView;
 
 public class CategoryManagement extends AppCompatActivity {
     private ListView mListView;
-
+    Button addCategoryActivityButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +28,14 @@ public class CategoryManagement extends AppCompatActivity {
         mListView=(ListView)findViewById(R.id.category_list);
         CategoryAdapter adapter = new CategoryAdapter(this);
         mListView.setAdapter(adapter);
-
+        addCategoryActivityButton=(Button) findViewById(R.id.go_to_add_category);
+        addCategoryActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddCategory.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
